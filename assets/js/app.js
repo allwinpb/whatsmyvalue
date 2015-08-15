af2e1bf08e1db675617ebb12df396c1792a50c08
Allwin Panikulam <allwinpb@gmail.com>
2015-08-16 01:23:46 +0800

    Moving map and geolocation

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,27 @@
 $(function(){
 	$('.button-row').css('min-height',$(window).height()/5);
-	var velocity = 0; angle = 0;
-	var moveMap = function(velocity, angle){
+	if (navigator.geolocation)
+		navigator.geolocation.getCurrentPosition(function(position) {
+			$.getJSON('http://ws.geonames.org/countryCode', {
+				lat: position.coords.latitude,
+				lng: position.coords.longitude,
+				type: 'JSON'
+			}, function(result) {
+				alert(result.countryName);
+			});
+		});
+
+	//Move the map on the location button
+	var velocity = 1; angle = 0;
+	var moveMap = function(){
+		var xy = $('.location-holder').css('background-position');
+		var x = parseInt(xy.split(' ')[0].split('px')[0]);
+		var y = parseInt(xy.split(' ')[1].split('px')[0]);
+		var dx = x + velocity*Math.cos(angle) + 'px';
+		var dy = y + velocity*Math.sin(angle) + 'px';
+		$('.location-holder').css('background-position',dx + ' ' + dy);
+		// velocity = velocity + Math.random()*2 - 1;
+		// angle = angle + (Math.random()*10 - 5)*3.14/180;
 	}
+	setInterval(moveMap, 10);
 })
