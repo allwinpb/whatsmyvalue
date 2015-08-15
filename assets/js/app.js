1aa5b166e21cb992d95bc07b119046b9277da234
Allwin Panikulam <allwinpb@gmail.com>
2015-08-16 00:30:22 +0800

    Second commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,6 @@
 $(function(){
-	$('.button-row').css('min-height',$(window).height()/5)
+	$('.button-row').css('min-height',$(window).height()/5);
+	var velocity = 0; angle = 0;
+	var moveMap = function(velocity, angle){
+	}
 })
